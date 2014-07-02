9d34edbe788e9e0bb35efe0bfdfae6f06c28dad4
Elayabharath <elayabharath.elango@autodesk.com>
2014-07-02 14:45:28 +0800

    Added points support for Dynamo export

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
+using svgPort;
+
 namespace Application
 {
     class Program
@@ -12,53 +14,9 @@ namespace Application
         static void Main(string[] args)
         {
 
-            List<double> x = new List<double> { };
-            List<double> y = new List<double> { };
-
-            Random rand = new Random();
-            System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Users\\t_elane\\Desktop\\file.txt");
             
-            for (int i = 0; i < 100; ++i)
-            {
-                x.Add(rand.Next(0, 10));
-                file.WriteLine(x[i]);
-            }
-            file.WriteLine("Histogram");
-            List<int> hist = analysis.findHistogram(x, 5, 0, 10);
-            foreach (int elements in hist)
-            {
-                file.Write(elements + ", ");
-            }
-            
-            //double mean = analysis.findMean(x);
-            //file.WriteLine("Mean: " + mean);
-
-            //List<double> mode = analysis.findMode(x);
-            //file.WriteLine("Mode:");
-            //for (int i = 0; i < mode.Count; ++i)
-            //    file.Write(" " + mode[i]);
-            //file.WriteLine("\n");
-
-            //double stdDev = analysis.findStdDeviation(x);
-            //file.WriteLine("Std dev: " + stdDev);
-
-            //double vari = analysis.findVariance(x);
-            //file.WriteLine("Std Var: " + vari);
-
-            //double median = analysis.findMedian(x);
-            //file.WriteLine("Median: " + median);
-
-            //double cor = analysis.sfindCorrelation(x, y);
-            //file.WriteLine("Correlation: " + cor);
-
-            //double pcor = analysis.findPearsonCorrelation(x, y);
-            //file.WriteLine("Pearson: " + pcor);
-            
-
-            file.Close();
-
-            //Console.ReadKey();
-
+            System.Console.WriteLine("file written");
+            System.Console.ReadKey();
         }
     }
 }
--- svgPort/svg.cs
+++ svgPort/svg.cs
@@ -3,29 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
 
 namespace svgPort
 {
-    public class svg
+    public class SVG
     {
-        private static void CreateNewSVGFile(String filePath, String fileName)
+        private static System.IO.StreamWriter CreateNewSVGFile(String filePath, String fileName)
         {
             System.IO.StreamWriter file = new System.IO.StreamWriter(filePath + fileName + ".SVG");
+            return file;
         }
 
-        private static void preSVGBody()
+        private static void preSVGBody(System.IO.StreamWriter file)
         {
+            String line1 = @"<?xml version=""1.0"" encoding=""iso-8859-1""?>";
+            String line2 = "<!-- Generator: Dynamo SVG Export Addon. visit www.dynamobim.org  -->";
+            String line3 = "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">";
 
+            file.WriteLine(line1);
+            file.WriteLine(line2);
+            file.WriteLine(line3);
         }
 
-        private static void postSVGBody()
+        
+        public static void exportPathsAsSVG(List<Geometry> geometry, String exportLocation, string fileName )
         {
+            
+            //TODO: Handle replication for geometry
 
-        }
+            var file = CreateNewSVGFile(exportLocation, fileName);
+            
+            //fill the SVG headers
+            preSVGBody(file);
+            
+            //start the svg tag
+            file.WriteLine(@"<svg version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" x=""0px"" y=""0px"" width=""1000px"" height=""1000px"" viewBox=""0 0 1000 1000"" xml:space=""preserve""> ");
+            
 
-        public static void exportPathsAsSVG()
-        {
+            //segregate all points
+            List<Point> pts = new List<Point>();
+            for (int i = 0; i < geometry.Count; ++i)
+            {
+                if (geometry[i].GetType() == typeof(Point))
+                {
+                    pts.Add((Point)geometry[i]);
+                }
+            }
 
+            //write all points into the file
+            //TODO: currently Z values are ignored, need a better way to do this
+            //TODO: segregate points by layers
+            for (int i = 0; i < pts.Count; ++i)
+            {
+                double x = pts[i].X;
+                double y = pts[i].Y;
+
+                file.WriteLine("<circle cx='"+x.ToString()+"' cy='"+y.ToString()+"' r='1' fill='black'/>");
+            }
+
+            //complete the svg tag
+            file.WriteLine("</svg>");
+            file.Close();
         }
     }
 }
